2c6d8b3e7e4131b7698437dda7d45dd70f24342b
Allan Egidio <allan.egidio@outlook.com>
2022-03-21 18:05:34 -0300

    playing with rust

---
+++ variable/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    /*
+        Playing with mutable variables.
+    */
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+
+    // Integers
+    let a = 98_222;      //Decimal
+    let b = 0xff;        //Hex
+    let c = 0o77;        //Octal 
+    let d = 0b1111_0000; //Binary
+    let e = b'A';         //Byte (u8 only)
+
+    let f: u8 = 255;
+
+    // Floating Numbers
+}
